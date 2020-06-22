2a8fb5d5d655408df00c7adf9fdfd3f0beab4380
Viictorreiss <victorquintareis@gmail.com>
2020-06-22 11:10:58 -0300

    produto

---
+++ produto/migrations/0004_auto_20190815_1039.py
@@ -0,0 +1,18 @@
+
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('produto', '0003_auto_20190814_2044'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='produto',
+            name='tipo',
+            field=models.CharField(choices=[('V', 'Variável'), ('S', 'Simples')], default='V', max_length=1),
+        ),
+    ]
